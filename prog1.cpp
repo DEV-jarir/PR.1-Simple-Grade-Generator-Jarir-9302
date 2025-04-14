3d4e5089a44e9786c6d3e6e1558b133f74002feb
dev-jarir08 <your-email-address>
2025-04-14 11:14:07 +0530

    update

--- prog1.cpp
+++ prog1.cpp
@@ -18,21 +18,41 @@ int main()
     :(mark >= 33) 
     ? 'D' 
     : 'F';
+    (marks <= 100 && marks > 0)
+    ? cout << "Your grade is: " << grade << ". "
+    : cout << "Invalid marks" << endl;
+
 
     switch(grade){
-    case 'A':cout<<"Your Grade is A. Excellent Work! You are Eligible for next levl.";
-    break;
-    case 'B':cout<<"Your Grade is B. Good Work ! You are Eligible for next levl.";
-    break;
-    case 'C':cout<<"Your Grade is C. Avrege Work ! You are Eligible for next levl.";
-    break;
-    case 'D':cout<<"Your Grade is D. Work Hard ! You are Eligible for next levl.";
-    break;
-    case 'F':cout<<"Your Grade is F ! Please try again next time.";
-    break;
-    default:"invalid grade.";
+      case 'A':cout<<"Your Grade is A. Excellent Work!";
+      break;
+
+    
+      case 'B':cout<<"Your Grade is B. Good Work !";
+      break;
+
+      case 'C':cout<<"Your Grade is C. Avrege Work !";
+      break;
+
+      case 'D':cout<<"Your Grade is D. Work Hard !";
+      break;
+
+      case 'F':cout<<"Your Grade is F !";
+      break;
+      
+      default:"invalid grade.";
+    }
+
+    if (grade == 'A' || grade == 'B' || grade == 'C' || grade == 'D')
+    {
+        cout << " You are eligible for the next level of your study. " << endl;
+    }
+    else
+    {
+        cout << "Please try again next time." << endl;
+    }
 
     return 0;   
   }
 
-}
+
